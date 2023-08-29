ac9ab2e10a0b3ea1957dac68680e17e774920413
FDRosayl <fdrosayl@gmail.com>
2023-08-29 08:53:27 -0700

    task 2 added

---
+++ task2.ts
@@ -0,0 +1,42 @@
+//calculate area of rectangle
+console.log(userName);
+ var length:number = 10;
+ var width:number = 20;
+ var area:number = length*width;
+ console.log(area);
+
+  //calculate volume of a cube.
+var l:number = 12;
+var volume:number = l*l*l;
+console.log(volume);
+
+//calculate the result of mathematical expression
+var mathExp:number = ((10+5)*3-2)/((4%3)-7);
+console.log(mathExp);
+
+//print if given number is positive or negative
+
+var givenNumber:number = 20;
+if(givenNumber>0){  
+console.log("givenNumber is positive");
+}
+else if(givenNumber<0){
+console.log("givenNumber is negative");    
+}  
+else{console.log("givenNumber is zero");
+}
+
+// write a program that determine if a person eligible to vote based on their age
+var candidateAge:number = 20;
+if (candidateAge>=18){
+console.log("congratulation you can cast vote");  
+}
+else{console.log("You are not eligible");
+}
+
+// write a program that determine if a number is even or odd
+var providedNumber:number = 20;
+if(providedNumber%2 ==0){console.log("providedNumber is even");
+} 
+else{console.log("providedNumber is odd");
+}
